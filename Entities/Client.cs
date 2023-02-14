1eefbc3c14156249e03bb83a8eaac0461bdefc07
Matheus Vital <matheusvital48@gmail.com>
2023-02-14 11:59:15 -0300

    Inicialização das classes e do programa main

---
+++ Entities/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioFixacao.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+
+        public Client() 
+        {
+        }
+
+        public Client(string name, string email, DateTime date)
+        {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using ExercicioFixacao.Entities;
+using ExercicioFixacao.Entities.Enums;
+namespace ExercicioFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data: ");
+            OrderStatus orderData = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.WriteLine("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
